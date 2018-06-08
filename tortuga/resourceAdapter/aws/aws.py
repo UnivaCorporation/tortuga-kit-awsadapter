6eb56fb970d381deda311b6e33addd4695aafb80
Mike Frisch <mfrisch@univa.com>
2018-06-08 11:32:57 -0400

    Minor fix VPC DNS domain lookup

--- tortuga/resourceAdapter/aws/aws.py
+++ tortuga/resourceAdapter/aws/aws.py
@@ -555,7 +555,8 @@ class Aws(ResourceAdapter):
         """
 
         try:
-            vpcconn = boto.vpc.connect_to_region(region,
+            vpcconn = boto.vpc.connect_to_region(
+                config['region'],
                 aws_access_key_id=config['awsaccesskey'],
                 aws_secret_access_key=config['awssecretkey']
             )
