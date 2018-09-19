3138faa2a10df44d197c0d1ca6bfc7264a6be0b1
Mike Frisch <mfrisch@univa.com>
2018-09-19 08:20:32 -0400

    Use 'valid' AMI ID in unit tests

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -90,7 +90,7 @@ def test_deleteNode(load_config_dict_mock, dbm):
 @mock.patch.object(Aws, '_load_config_from_database')
 @mock_ec2_deprecated
 def test_start(load_config_dict_mock, pre_add_host_mock,
-               fire_provisioned_even_mock, dbm):
+               fire_provisioned_even_mock, dbm, valid_ami):
     """
     Test ResourceAdapter.start() workflow
     """
@@ -99,7 +99,7 @@ def test_start(load_config_dict_mock, pre_add_host_mock,
         'awsAccessKey': 'the_key',
         'awsSecretKey': 'the_secret',
         'keypair': 'the_keypair',
-        'ami': 'ami-abcd1234',
+        'ami': valid_ami,
         'use_instance_hostname': 'true',
         'instancetype': 'the_instancetype'
     }
@@ -147,11 +147,11 @@ def test_start(load_config_dict_mock, pre_add_host_mock,
 @mock.patch.object(Aws, '_load_config_from_database')
 @mock_ec2_deprecated
 def test_start_update_node(load_config_dict_mock, pre_add_host_mock,
-                           fire_provisioned_event_mock, dbm):
+                           fire_provisioned_event_mock, dbm, valid_ami):
     configDict = {
         'awsAccessKey': 'the_key',
         'awsSecretKey': 'the_secret',
-        'ami': 'ami-abcd1234',
+        'ami': valid_ami,
         'use_instance_hostname': 'true',
     }
 
